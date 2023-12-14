8b1a09d04056eb15a357539b29564a71da4a13e6
Pedro Silva <pedrohenriquefesi@gmail.com>
2023-12-13 23:14:48 -0300

    cores corrigidas

--- src/components/MyStudies.tsx
+++ src/components/MyStudies.tsx
@@ -13,7 +13,7 @@ export const MyStudies = ({ closeMyStudies }: Props) => {
                     <IconClose width={24} height={24} className="text-white" />
                 </div> */}
                 <div className="px-6 py-4 border-b border-white/20">
-                    <h1 className="mb-1 text-lg font-bold">Bem vindo a IntellectIA</h1>
+                    <h1 className="mb-1 text-lg font-bold text-white">Bem vindo a IntellectIA</h1>
                     <h2 className="text-sm text-white/80">Recursos da plataforma</h2>
                 </div>
                 <div className="p-6">
@@ -21,21 +21,21 @@ export const MyStudies = ({ closeMyStudies }: Props) => {
                         <div className="border-intellectia-lightblue rounded-md mb-4 flex-1">
                             <div className="flex flex-rol items-center mb-2">
                                 <div className="w-3 h-3 rounded-full bg-green-600"></div>
-                                <h2 className="font-bold ml-2 text-sm">Chat personalizado</h2>
+                                <h2 className="font-bold ml-2 text-sm text-white">Chat personalizado</h2>
                             </div>
                             <p className="text-sm text-white/80">Nossa plataforma possui um chat com inteligência artificial projetado especificamente para te auxiliar nos estudos para o vestibular.</p>
                         </div>
                         <div className="border-intellectia-lightblue rounded-md mb-4 flex-1">
                             <div className="flex flex-rol items-center mb-2">
                                 <div className="w-3 h-3 rounded-full bg-blue-600"></div>
-                                <h2 className="font-bold ml-2 text-sm">Pesquisa na web</h2>
+                                <h2 className="font-bold ml-2 text-sm text-white">Pesquisa na web</h2>
                             </div>
                             <p className="text-sm text-white/80">Com esse recurso, você poderá fazer perguntas sobre qualquer assunto recente para economizar o seu tempo de pesquisa na internet.</p>
                         </div>
                         <div className="border-intellectia-lightblue rounded-md mb-4 flex-1">
                             <div className="flex flex-rol items-center mb-2">
                                 <div className="w-3 h-3 rounded-full bg-purple-600"></div>
-                                <h2 className="font-bold ml-2 text-sm">Busca de questões</h2>
+                                <h2 className="font-bold ml-2 text-sm text-white">Busca de questões</h2>
                             </div>
                             <p className="text-sm text-white/80">Não perca mais tempo procurando questões na internet, aqui você pode pedir questões sobre diversos assuntos e ter um feedback imediato da resposta.</p>
                         </div>
@@ -43,8 +43,8 @@ export const MyStudies = ({ closeMyStudies }: Props) => {
 
                     <div className="flex flex-row justify-end">
                         <div onClick={() => closeMyStudies()} className="flex w-40 h-10 justify-center items-center rounded-md md:hover:brightness-75 bg-green-600 cursor-pointer transition-all">
-                            <p className="font-bold text-lg">Começar</p>
-                            <IconPlayFill width="20" height="20" className="ml-2"></IconPlayFill>
+                            <p className="font-bold text-lg text-white">Começar</p>
+                            <IconPlayFill width="20" height="20" className="ml-2" color="white"></IconPlayFill>
                         </div>
                     </div>
                 </div>
