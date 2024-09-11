a604ec7697cd77d4823878b58b7a9cef263058d6
Pedro Henrique <pedrohenriquefesi@gmail.com>
2024-09-11 12:27:32 -0300

    header fixo

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -16,15 +16,18 @@ export default function Page() {
         <DashboardPageHeaderTitle className="text-2xl">Estudo dirigido</DashboardPageHeaderTitle>
         <div className="w-8 h-8"></div>
       </DashboardPageHeader>
-      <DashboardPageMain className="bg-intellectia-gray flex justify-center">
-        <div className="md:max-w-6xl">
-          <ChatSubjectArea subject="Biologia" bgColor={"bg-green-300/30"} />
-          <ChatSubjectArea subject="Geografia" bgColor={"bg-blue-300/30"} />
-          <ChatSubjectArea subject="Filosofia e Sociologia" bgColor={"bg-orange-300/30"} />
-          <ChatSubjectArea subject="Inglês" bgColor={"bg-yellow-200/30"} />
-          <ChatSubjectArea subject="Português" bgColor={"bg-purple-300/30"} />
-          <ChatSubjectArea subject="História" bgColor={"bg-orange-900/30"} />
-        </div>
+      
+      <DashboardPageMain className="flex flex-1 overflow-auto">
+        <section className="bg-intellectia-gray flex justify-center w-full">
+          <div className="md:max-w-6xl">
+            <ChatSubjectArea subject="Biologia" bgColor={"bg-green-300/30"} />
+            <ChatSubjectArea subject="Geografia" bgColor={"bg-blue-300/30"} />
+            <ChatSubjectArea subject="Filosofia e Sociologia" bgColor={"bg-orange-300/30"} />
+            <ChatSubjectArea subject="Inglês" bgColor={"bg-yellow-200/30"} />
+            <ChatSubjectArea subject="Português" bgColor={"bg-purple-300/30"} />
+            <ChatSubjectArea subject="História" bgColor={"bg-orange-900/30"} />
+          </div>
+        </section>
       </DashboardPageMain>
     </DashboardPage>
   );
